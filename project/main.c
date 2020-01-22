0def014e6a841210e60157b8475af71a8166e95c
Wojciech Korzybski <46850894+Grzybu1@users.noreply.github.com>
2020-01-22 13:34:48 +0100

    Add files via upload

--- project/main.c
+++ project/main.c
@@ -7,26 +7,27 @@
 #include "stdio.h"
 //#include "mbed.h"
 
+int debug = 0;
 volatile uint32_t ticks = 0;	// zmienna do delay
-uint32_t randomTick = 5000;
-double alfaRandom = 1.2435;
+uint32_t randomTick = 2124;
+double alfaRandom = 1.02435;
 volatile unsigned int I2C_Timeout = 0;
 
 int readId = 0;	
 int writeId = 0;
 //Tu bedziemy dostawac dane
-volatile unsigned char buforRead[20];
+volatile uint16_t buforRead[20];
 //Tu nalezy zapisac adres slave'a razem z bitem R/W
-volatile unsigned char buforWrite[20];
+volatile uint16_t buforWrite[20];
 //Tu wrzucamy ile bajt�w danych sie spodziewamy
 volatile int maxRead; 
 //Tu wrzucamy ile bajt�w danych wysylamy
 volatile int maxWrite;
 
 short int side = 10;
-uint8_t snakeSize = 1;
+uint16_t snakeSize = 1;
 uint8_t bestScores[20] = {0};
-bestScoresSize 20;
+uint16_t bestScoresSize = 20;
 
 short int xTab[32] = {0};
 short int yTab[20] = {0};
@@ -41,15 +42,79 @@ char coloredTab[640];
 uint8_t tailDirection[1000] = {2};
 short int direction = 2;
 short int currentTail = 0;
+void I2C_transmit();
+void accelerometr_read()
+{
+	//Zakładam że przy wielobajtowym odczycie akcelerometr przesyła bajty z kolejno występujących po sobie rejestrach. 
+	//Jeśli nie zadziała to trzeb to rozbić na  osobne transmisje.
+	maxRead = 4;
+	maxWrite = 3;
+	for (int i = 0; i < 4; ++i)
+	{
+		buforRead[i] = 0;
+	}
+	buforWrite[0] = 0x3A; //3A adres urządzenia z bitem write/read -> 0
+	buforWrite[1] = 0x32;	//Adres rejestru za wartością odczytaną X(1 z 2 bajtów)
+	buforWrite[2] = 0x3B; //adres urządzenia (0x1D) z bitem write/read -> 1
+	I2C_transmit();
+	delayMS(5);
+	randomTick += buforRead[0] + buforRead[1] + buforRead[2] + buforRead[3];
+	randomTick *= alfaRandom;
+	if(randomTick > 10000){
+		randomTick = buforRead[0] + buforRead[1] + buforRead[2] + buforRead[3];
+		randomTick *= alfaRandom;
+	
+	}
+	//w bufor read mamy kolejno - mniej znaczący bajt X, bardziej znaczący bajt X,
+	//mniej znaczący bajt Y, bardziej znaczący bajt Y
+}
+
+
+void configureUART0()
+{
+	LPC_UART0->LCR = 0x83;
+	LPC_UART0->DLL = 13;
+	LPC_UART0->DLM = 0;
+	LPC_UART0->FDR = 0xF1;
+	LPC_UART0->LCR = 0x03;
+
+	PIN_Configure(0, 2, 1, 0, 0); 
+	PIN_Configure(0, 3, 1, 0, 0); 
+}
+
+
+void sendString(char* str, short int length)
+{
+	for(int i = 0; i < length; i++)
+	{
+		/*THRE is set immediately upon detection of an empty UARTn THR and is cleared on a UnTHR write.
+1
+0 UnTHR contains valid data.
+1 UnTHR is empty.*/
+		while(!(LPC_UART0->LSR & 1 << 5));
+		LPC_UART0->THR = str[i];
+	}
+}
+
+void sendIntString(int num)
+{
+	char tmp[32] = {" "};
+	sprintf(tmp, "%d", num);
+	sendString(tmp, 32);
+}
+
 
 void SysTick_Handler(void)
 {
 	++ticks;
-	for(int i = 0; i < randomTick%20; i++){
-		randomTick = (int)((double)randomTick * alfaRandom);
-	}
-	if(randomTick > 31600100)
-		randomTick = randomTick/(alfaRandom*0.0000002);
+	/*if((ticks % 1000) == 0)
+	{
+		for(int i = 0; i < randomTick%20; i++){
+			randomTick = (int)((double)randomTick * alfaRandom);
+		}
+		if(randomTick > 10000)
+			randomTick = randomTick/(alfaRandom*0.0000002);
+	}*/
 }
 
 void delayMS(int ms) //miliseconds
@@ -105,13 +170,15 @@ void I2C_transmit()
 	//informacje ile juz odebralismy.
 	readId = 0;
 	writeId = 0;
-
+	if(debug == 1) sendString("transmit ", 9);
 	//Ustawienie bitu STA -> ustawienie flagi startu
 	LPC_I2C0->I2CONSET = 1<<5;
 	//Po ustawieniu flagi (teoretycznie) uruchamia sie przerwanie z kodem 0x08 w I2STAT
 
 	//Czyscimy flage startu tak na wszelki wypadek
 	LPC_I2C0->I2CONCLR = 1<<5;
+	
+	
 }
 
 
@@ -120,11 +187,11 @@ extern void I2C0_IRQHandler(void)
 {
 	//W tej zmiennej mamy aktualny status transmisji kt�ry nalezy obsluzyc.
 	unsigned char status = LPC_I2C0->I2STAT;
-
 	switch(status)
 	{
 		//Wyslano pojedynczy sygnal startu i otrzymano ACK. Po tym idziemy do 0x40 lub 0x48(odczyt), 0x18 lub 0x20(zapis)
 		case 0x08:
+			if(debug == 1) sendString("start ", 6);
 			//zapisujemy adres do wyslania - UWAGA! Adres musi posiadac na koncu bit W/R
 			LPC_I2C0->I2DAT = buforWrite[writeId];
 
@@ -142,6 +209,7 @@ extern void I2C0_IRQHandler(void)
 		//robimy w zasadzie to co przy 0x08. Jesli jestesmy w trybie reciever i wrzucimy 
 		//adres z bitem W to przejdziemy do trybu transmiter i na odwr�t
 		case 0x10:
+			if(debug == 1) sendString("repeated start ", 15);
 			//zapisujemy adres do wyslania - UWAGA! Adres musi posiadac na koncu bit W/R
 			LPC_I2C0->I2DAT = buforWrite[writeId];
 
@@ -157,6 +225,7 @@ extern void I2C0_IRQHandler(void)
 
 		//wyslano sla+w, otrzymano ACK. Wyslemy 1 bajt.
 		case 0x18:
+			if(debug == 1) sendString("slaw -> ack ", 12);
 			//ladujemy cos do wyslania
 			LPC_I2C0->I2DAT = buforWrite[writeId];
 			
@@ -173,6 +242,7 @@ extern void I2C0_IRQHandler(void)
 
 		//Wyslano SLA + w, otrzymano not ACK. Wysylamy stop
 		case 0x20:
+			if(debug == 1) sendString("slaw -> notack ", 15);
 			//ustawiamy flagi aa i sto
 			LPC_I2C0->I2CONSET = 0x14;
 
@@ -184,9 +254,11 @@ extern void I2C0_IRQHandler(void)
 		//Jesli tak to ostatni bajt jest adresem sla + R,
 		//wiec powinnismy wyslac najpierw ponowiony warunek start a dopiero p�zniej zawartosc ostatniej kom�rki bufora write.
 		case 0x28:
+			if(debug == 1) sendString("wyslano bajt -> ack -> ", 23);
 			//jesli kolejny bajt nie bedzie ostatnim to:
 			if((writeId + 1) < maxWrite)
 			{
+				if(debug == 1) sendString("wysyłam nie ostatni bajt ", 27);
 				//ladujemy cos do wyslania
 				LPC_I2C0->I2DAT = buforWrite[writeId];
 				
@@ -201,6 +273,7 @@ extern void I2C0_IRQHandler(void)
 				//zostalo cos do odczytu
 				if(readId < maxRead)
 				{
+					if(debug == 1) sendString("wracam do odczytu ", 18);
 					//Ustaw flage startu -> przejdzie do stanu 10 zeby wyslac ostatni element bufor write czyli sla + r
 					LPC_I2C0->I2CONSET = 1<<5;
 				}
@@ -210,6 +283,7 @@ extern void I2C0_IRQHandler(void)
 					//Jesli tak to nie jest to adres a raczej ostatni bajt konfiguracyjny przeznaczony do wyslania.
 					if(writeId < maxWrite)
 					{
+						if(debug == 1) sendString("wysylam ostatni ", 16);
 						//ladujemy cos do wyslania
 						LPC_I2C0->I2DAT = buforWrite[writeId];
 				
@@ -221,6 +295,7 @@ extern void I2C0_IRQHandler(void)
 					}
 					else
 					{
+						if(debug == 1) sendString("koniec", 6);
 						//ustawiamy flagi aa i sto
 						LPC_I2C0->I2CONSET = 0x14;
 					}
@@ -232,6 +307,7 @@ extern void I2C0_IRQHandler(void)
 
 		//Wyslano dane, otrzymano notACK. wysylamy stop
 		case 0x30:
+			if(debug == 1) sendString("wyslano bajt -> notack ", 23);
 			//ustawiamy flagi aa i sto
 			LPC_I2C0->I2CONSET = 0x14;
 
@@ -242,6 +318,7 @@ extern void I2C0_IRQHandler(void)
 		//stracono arbitraz podczas wysylania danych lub sla+w. 
 		//Transmitujemy start kiedy magistralawolna
 		case 0x38:
+			if(debug == 1) sendString("stracono arbitraz ", 18);
 			//ustawiamy flagi aa i sta
 			LPC_I2C0->I2CONSET = 0x24;
 
@@ -251,20 +328,22 @@ extern void I2C0_IRQHandler(void)
 
 		//Po wyslaniu w 0x08 SLA + R otrzymalismy ACK
 		case 0x40:
-
+			if(debug == 1) sendString("slar -> ack ", 12);
 			//jesli czekamy na wiecej niz bajt danych to oznaczamy ack do wyslania (po kolejnym otrzymanym 
 			//bajcie), jesli to bedzie ostatni bajt to po nim wyslemy NOT ACK
 			if((readId + 1) < maxRead)
 				LPC_I2C0->I2CONSET = 0x04; //Idziemy do 0x50
 			else
 				LPC_I2C0->I2CONCLR = 0x04;	//Idziemy do 0x58
-
+	
+			LPC_I2C0->I2CONCLR = 1<<5;
 			//Czyscimy bit SI
 			LPC_I2C0->I2CONCLR = 0x08;
 		break;
 
 		//Otrzymalismy not ACK po wyslaniu SLA + R wiec nalezy przerwac transmisje
 		case 0x48:
+			if(debug == 1) sendString("slar -> notack ", 15);
 			//Ustawiamy flagi AA i STO, zeby przerwac transmisje
 			LPC_I2C0->I2CONSET = 0x14;
 
@@ -274,6 +353,7 @@ extern void I2C0_IRQHandler(void)
 
 		//Po odebraniu bajtu danych odpowiedzilismy ack - po odebraniu danych czekamy na wiecej
 		case 0x50:
+			if(debug == 1) sendString("dostano dane -> ack ", 20);
 			//Odczytujemy otrzymane dane przyslane ze slave'a i zapisujemy do bufora
 			buforRead[readId] = LPC_I2C0->I2DAT;
 
@@ -292,6 +372,7 @@ extern void I2C0_IRQHandler(void)
 
 		//Po odebraniu bajtu danych odpowiedzilismy not ack co znaczy ze nie chcemy wiecej danych
 		case 0x58:
+			if(debug == 1) sendString("odebrano -> notack ", 19);
 			//Odczytujemy otrzymane dane przyslane ze slave'a i zapisujemy do bufora
 			buforRead[readId] = LPC_I2C0->I2DAT;
 
@@ -480,6 +561,8 @@ void printScoresBoard(){
 		drawString(120, 170-i*18, liczba, 2, LCDBlack);
 		drawString(140, 170-i*18, wynikStr, 2, LCDBlack);
 	}
+	while(Joystick_GetState() != JOYSTICK_CENTER){
+	}
 
 }
 
@@ -506,13 +589,15 @@ void generateFood()
   short int generated = 0;
   while(generated == 0)
   {
-    int indexX = ((float)randomTick/31600100.0)*32;
-		int indexY = ((float)randomTick/31600100.0)*20;
-		
-    if((coloredTab[32*indexY+indexX] != 'S') && (coloredTab[32*indexY+indexX] != 'T'))
+    int indexX = ((float)randomTick/10000.0)*32.;
+		int indexY = ((float)randomTick/10000.)*20.;
+		/*sendIntString(indexX);
+		sendIntString(indexY);
+		sendIntString(randomTick);*/
+    if((coloredTab[32*indexY+indexX] != 'S') && (coloredTab[32*indexY+indexX] != 'T') && (coloredTab[32*indexY+indexX] != 'F'))
     {
       coloredTab[32*indexY+indexX] = 'F';
-      drawCubeFromCenter(indexX, indexY, LCDGreen);
+      drawCubeFromCenter(xTab[indexX], yTab[indexY], LCDGreen);
       generated = 1;
     }
   }
@@ -565,7 +650,7 @@ short int updatePosition()
   }
   else
   {
-    drawCubeFromCenter(xTab[headTail[1][0]], yTab[headTail[1][1]], LCDBlueSea);
+    drawCubeFromCenter(xTab[headTail[1][0]], yTab[headTail[1][1]], LCDWhite);
     coloredTab[32 * headTail[1][1] + headTail[1][0]] = '0';
     switch (tailDirection[currentTail])
     {
@@ -601,7 +686,13 @@ short int updatePosition()
 
 void updateDirection(void)
 {
-  
+  accelerometr_read();
+	sendIntString(buforRead[0] + buforRead[1]);
+	sendString("  ",2);
+	sendIntString(buforRead[2] + buforRead[3]);
+	sendString("\n",2);
+	delayMS(5);
+	int left = 0;
   if(Joystick_GetState() == JOYSTICK_UP)
     direction = 0;
   if(Joystick_GetState() == JOYSTICK_LEFT)
@@ -630,55 +721,52 @@ void playGame(void)
 
 void accelerometr_enable() //0x3A dla zapisu i 0x3B dla odczytu
 {
+	I2C_enable();
 	maxRead = 0;
 	maxWrite = 3;
-	buforWrite[0] = 0x1D << 1; //3A adres urządzenia z bitem write/read -> 0
+	buforWrite[0] = 0x3A; //3A adres urządzenia z bitem write/read -> 0
 	buforWrite[1] = 0x24; //0x24 - adres rejestru z progami, których przekroczenie wywoła przerwanie
 	buforWrite[2] = 0x04; //250 mg (wrzucamy liczbę. 62.5 * wrzucona liczba to próg)
 	I2C_transmit();
+	delayMS(10);
+	
 	maxRead = 0;
 	maxWrite = 3;
-	buforWrite[0] = 0x1D << 1; //3A adres urządzenia z bitem write/read -> 0
+	buforWrite[0] = 0x3A; //3A adres urządzenia z bitem write/read -> 0
 	buforWrite[1] = 0x27; // adres rejestru w którym sa dane jakie zdarzenia mają powodować przerwanie
 	buforWrite[2] = 0x60; // Włączenie przerwań na płaszczyznach x oraz y
 	I2C_transmit();
+	delayMS(10);
+	
 	maxRead = 0;
 	maxWrite = 3;
-	buforWrite[0] = 0x1D << 1; //3Aadres urządzenia z bitem write/read -> 0
+	buforWrite[0] = 0x3A; //3Aadres urządzenia z bitem write/read -> 0
 	buforWrite[1] = 0x2E; // adres rejestru w którym uruchamiamy przerwania
 	buforWrite[2] = 0x10; // Włączenie przerwań pojedynczych
 	I2C_transmit();
+	delayMS(10);
+	
+	maxRead = 0;
+	maxWrite = 3;
+	buforWrite[0] = 0x3A; //3Aadres urządzenia z bitem write/read -> 0
+	buforWrite[1] = 0x2D; // adres rejestru w którym uruchamiamy przerwania
+	buforWrite[2] = 0x08; // Włączenie przerwań pojedynczych
+	I2C_transmit();
+	delayMS(10);
 }
 
-void accelerometr_read()
-{
-	//Zakładam że przy wielobajtowym odczycie akcelerometr przesyła bajty z kolejno występujących po sobie rejestrach. 
-	//Jeśli nie zadziała to trzeb to rozbić na  osobne transmisje.
-	maxRead = 4;
-	maxWrite = 3;
-	for (int i = 0; i < 4; ++i)
-	{
-		buforRead[i] = 0;
-	}
-	buforWrite[0] = 0x1D << 1; //3A adres urządzenia z bitem write/read -> 0
-	buforWrite[1] = 0x32;	//Adres rejestru za wartością odczytaną X(1 z 2 bajtów)
-	buforWrite[2] = 0x3B; //adres urządzenia (0x1D) z bitem write/read -> 1
-	I2C_transmit();
-	//w bufor read mamy kolejno - mniej znaczący bajt X, bardziej znaczący bajt X,
-	//mniej znaczący bajt Y, bardziej znaczący bajt Y
-}
 
 void RTC_IRQHandler(void)
 {
 	// if(isTick)
 	// {
 	// 	isTick = 0;
-	// 	sendString("Tick ", 5);
+	// 	if(debug == 1) sendString("Tick ", 5);
 	// }
 	// else
 	// {
 	// 	isTick = 1;
-	// 	sendString("Tack ", 5);
+	// 	if(debug == 1) sendString("Tack ", 5);
 	// }
 	LPC_RTC->ILR = 1;
 
@@ -693,8 +781,11 @@ void configureRTC()
 	NVIC_EnableIRQ(RTC_IRQn);
 }
 
+
+
 int main(void)
 {
+	configureUART0();
 	configureRTC();
 	Buttons_Initialize();
 	Joystick_Initialize();
@@ -703,7 +794,7 @@ int main(void)
 	init_ILI9325();
 	lcdCleanBackground(LCDWhite);
 
-																			writeBestScores(); // to trzeba uruchomic tylko raz dla danej plytki
+	writeBestScores(); // to trzeba uruchomic tylko raz dla danej plytki
 	
 	/*maxWrite = 3;
 	buforWrite[0] = 0x1D << 1;
@@ -716,29 +807,29 @@ int main(void)
 	accelerometr_enable();
 	readBestScores(); // funkcja zapisujaca najlepsze wyniki gry do tablicy bestScores
 	char toDraw[50] = {'_'};
+	if(debug == 1) sendString("\n", 2);
 while(1)
 {
 	lcdCleanBackground(LCDWhite);
 	for(int i = 0; i < 50; i++)
 		toDraw[i] = '_';
-	maxWrite = 3;
-	buforWrite[0] = 0x1D << 1;
-	buforWrite[1] = 0x2E;
-	buforWrite[2] = (0x1D << 1) | 1;
-	maxRead = 1;
-	I2C_transmit();
-	//drawGUI();
-	sprintf(toDraw, "%d", buforRead[0]);
-	drawString(50,50,toDraw,50,LCDBlack);
-	delayMS(100);
 	
-	/*if(Joystick_GetState() == JOYSTICK_CENTER)
+	if(Joystick_GetState() == JOYSTICK_CENTER)
   {
 		drawGUI();
     playGame();
-  }*/
-  //drawLine(0, 0, 10, 320, LCDGrey);
-	//drawLetter(50, 50, 'K', LCDBlack);
+  }
+	/*accelerometr_read();
+	sendIntString(buforRead[0]);
+	sendString("  ",2);
+	sendIntString(buforRead[1]);
+		sendString("  ",2);
+	sendIntString(buforRead[2]);
+		sendString("  ",2);
+	sendIntString(buforRead[3]);
+		sendString("\n",2);
+	delayMS(100);*/
+
 	
 }
 
